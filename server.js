6178819d5964e121d17d7f49d9513524f8ba675b
Anant29-nex <anantsawantnexocide2025@gmail.com>
2025-08-05 00:06:44 +0530

    changed

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/auth");
+const postRoutes = require("./routes/posts");
 
 dotenv.config();
+const app = express();
+app.use(
+  cors({
+    origin: "https://frontend-two-fawn-23.vercel.app/",
+  })
+);
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+  .connect(process.env.MONGO_URI)
+  .then(() =>
+    app.listen(process.env.PORT || 5000, () => console.log("Server running"))
+  )
+  .catch((err) => console.error(err));
